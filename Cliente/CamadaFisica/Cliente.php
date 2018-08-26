1dec9a48b11a0da64a98467c4efe704cc5c72a3e
yullidias <yulli.dias@hotmail.com>
2018-08-26 14:04:32 -0300

    enviando e recebendo quadro

--- Cliente/CamadaFisica/Cliente.php
+++ Cliente/CamadaFisica/Cliente.php
@@ -6,23 +6,30 @@
  * Time: 9:37 PM
  */
 
-/*
-function getMAC($interface)
+function getMAC($ip)
 {
-    $arp_scan = shell_exec("arp-scan --interface=" . $interface . " --localnet"); //necessario executar como root
-    print $arp_scan;
-    $linhas = explode("\n", $arp_scan);
-    print "===\n";
-    print $linhas[3];
-    $ipAndMAC = explode("	", $linhas[3]);
-    $IP = $ipAndMAC[0];
-    $MAC = $ipAndMAC[1];
-    print $IP. $MAC;
-}*/
+    if($ip == '127.0.0.1')
+    {
+        return "d0:df:9a:c4:07:ab";
+    }
+    else
+    {
+        do
+        {
+            $arp_scan = shell_exec("arp-scan " . $ip); //necessario executar como root
+            $linhas = explode("\n", $arp_scan);
+            $array = str_split($linhas[2]);
+            $mac = '';
+            $i = 13;
+            while($i < strlen($linhas[2]) && $i <=29)
+            {
+                $mac = $mac . $array[$i];
+                $i++;
+            }
+        }while(strlen($mac) < 17);
+        return $mac;
+    }
 
-function getMAC()
-{
-    return "d0:df:9a:c4:07:ab";
 }
 
 function macParaBinario($mac)
@@ -75,13 +82,69 @@ function conectarAoServidor($host, $port, $mensagem, $limite)
     socket_close($socket);
     return $resposta;
 }
-$bin = macParaBinario(getMAC());
-binarioParaMac($bin);
+function string_to_bin($string){ //converte a string em uma sequencia binaria//
+    $bin = '';
+    $chars = str_split($string); //separa a string em um array de caracteres//
+    foreach($chars as $c){//para cada caractere na string//
+        $hex = unpack('H*', $c);//passa o caractere para hexadecimal//
+        $b = base_convert($hex[1], 16, 2); //passa o hexadecimal para binario//
+        while(strlen($b)<8){ $b = '0'.$b; } //garante que tem 8 bits//
+        $bin .= $b; //concatena//
+    } //echo "String: ".$string."</br>Binario: ".$bin."</br>";//debug//
+    return $bin;
+}
 
+function getMensagemPacote()
+{
+    $conteudo = file('../pacote.txt');
+    $split = explode(' ', $conteudo[0]);
+    return $split[1];
+}
+function getIpPacote()
+{
+    $conteudo = file('../pacote.txt');
+    $split = explode(' ', $conteudo[0]);
+    return $split[0];
+}
+function MontaQuadro()
+{
+    $ipDestino = getIpPacote();
+    $mensagem = getMensagemPacote();
+    $ipOrigem = file('../myIp.txt');
+    $ipOrigem = $ipOrigem[0];
 
+    $preambulo = '0101';
+    $sfd = '10101011'; // Delimitador de início de quadro //
+    $macOrigem = macParaBinario(getMAC($ipOrigem));
+    $macDestino = macParaBinario(getMAC($ipDestino));
+    $tipo = '0100100101010000';//IP//
+    $data = string_to_bin($mensagem); //converte o pacote para binário//
+    $crc = '01000101010100100101001001001111'; //string ERRO//
+    return $preambulo.$sfd.$macOrigem.$macDestino.$tipo.$data.$crc;
+}
+
+function bin_to_string($bin){ //converte a sequencia binaria para uma string//
+    $string = '';
+    for($i=0; $i<(strlen($bin)-1); $i+=8){//para cada caractere em binário//
+        $hex = base_convert(substr($bin, $i, 8), 2, 16);//converte de binário para hexadecimal//
+        while(strlen($hex)<2){ $hex = '0'.$hex; }
+        $c = pack('H*', $hex);//passa o hexadecimal para caractere, concatena na string//
+        $string .= $c;
+    } //echo "Binario: ".$bin."<</br>>String: ".$string."<</br>>";//debug//
+    return $string;
+}
 //=========================
 $host    = "127.0.0.1";
 $port    = 8080;
 
-$tamMensagem = conectarAoServidor($host,$port,"TAM", 1024);
-conectarAoServidor($host,$port, "Teste", $tamMensagem);
+$quadro = MontaQuadro();
+print $quadro ."\n";
+
+$tamMensagemEmBinario = conectarAoServidor($host,$port,string_to_bin("TAM"), 1024);
+$tamMensagem = bin_to_string($tamMensagemEmBinario);
+$mensagem = MontaQuadro();
+$resposta = conectarAoServidor($host,$port, $mensagem, $tamMensagem);
+if(strcmp($resposta, $mensagem) == 0)
+{
+    print "Pacote recebido com sucesso!";
+}
--- Servidor/CamadaFisica/Servidor.php
+++ Servidor/CamadaFisica/Servidor.php
@@ -7,35 +7,71 @@
  */
 
 //===========
-$TAM_MAX_BYTES = 3000000;
+
+function bin_to_string($bin){ //converte a sequencia binaria para uma string//
+    $string = '';
+    for($i=0; $i<(strlen($bin)-1); $i+=8){//para cada caractere em binário//
+        $hex = base_convert(substr($bin, $i, 8), 2, 16);//converte de binário para hexadecimal//
+        while(strlen($hex)<2){ $hex = '0'.$hex; }
+        $c = pack('H*', $hex);//passa o hexadecimal para caractere, concatena na string//
+        $string .= $c;
+    } //echo "Binario: ".$bin."<</br>>String: ".$string."<</br>>";//debug//
+    return $string;
+}
+function string_to_bin($string){ //converte a string em uma sequencia binaria//
+    $bin = '';
+    $chars = str_split($string); //separa a string em um array de caracteres//
+    foreach($chars as $c){//para cada caractere na string//
+        $hex = unpack('H*', $c);//passa o caractere para hexadecimal//
+        $b = base_convert($hex[1], 16, 2); //passa o hexadecimal para binario//
+        while(strlen($b)<8){ $b = '0'.$b; } //garante que tem 8 bits//
+        $bin .= $b; //concatena//
+    } //echo "String: ".$string."</br>Binario: ".$bin."</br>";//debug//
+    return $bin;
+}
+function MontaPacote($quadro){
+    $preambulo = substr($quadro, 0, 4); //4 bits//
+    $sfd = substr($quadro, 4, 8); //8 bits//
+    $mac_org = substr($quadro, 12, 48); //echo bin_to_mac($mac_org)."</br>";//
+    $mac_dest = substr($quadro, 60, 48);  //echo bin_to_mac($mac_dest)."</br>";//
+    $tipo = substr($quadro, 108, 16); //16 bits//IP//
+    $tam_dado = strlen($quadro) - 156; //tamanho total - cabeçalho - crc//
+    $data = substr($quadro, 124, $tam_dado);
+    $data = bin_to_string($data); //converte o pacote para string//
+    $crc = substr($quadro, 124+$tam_dado, 32); //crc tem 32 bits//string ERRO//
+    return $data;
+}
+
 // set some variables
 $host = "127.0.0.1";
 $port = 8080;
+$TAM_MAX_BYTES = '3000000';
 // don't timeout!
 set_time_limit(0);
 // create socket
 $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
 // bind socket to port
-$result = socket_bind($socket, $host, $port) or die("Could not bind to socket\n");
+socket_bind($socket, $host, $port) or die("Could not bind to socket\n");
 // start listening for connections
-$result = socket_listen($socket, 3) or die("Could not set up socket listener\n");
+socket_listen($socket, 3) or die("Could not set up socket listener\n");
 $spawn = socket_accept($socket) or die("Could not accept incoming connection\n");
 while ($spawn != FALSE)
 {
     // read client input
-    $input = socket_read($spawn, 1024) or die("Could not read input\n");
+    $quadro = socket_read($spawn, intval($TAM_MAX_BYTES)) or die("Could not read input\n");
     // clean up input string
-    $input = trim($input);
-    echo "\nClient Message : ".$input;
-    if(strcmp($input, "TAM") == 0)
+    $quadro = trim($quadro);
+    print "quadro " . $quadro . "\n";
+    $mensagem = bin_to_string($quadro);
+    if(strcmp($mensagem, "TAM") == 0)
     {
-        socket_write($spawn, $TAM_MAX_BYTES, strlen ($TAM_MAX_BYTES)) or die("Could not write output\n");
+        $resposta = string_to_bin($TAM_MAX_BYTES);
+        socket_write($spawn, $resposta, strlen ($resposta)) or die("Could not write output\n");
     }
     else
     {
-        // reverse client input and send back
-        $output = strrev($input) . "\n";
-        socket_write($spawn, $output, strlen ($output)) or die("Could not write output\n");
+        print "MP ". MontaPacote($quadro) . "\n\n";
+        socket_write($spawn, $quadro, strlen ($quadro)) or die("Could not write output\n");
     }
 
     $result = socket_listen($socket, 3) or die("Could not set up socket listener\n");
