0a9b47dda3fd9c0bfe24684129e041b175f580a6
BulgarelliM <42696123+BulgarelliM@users.noreply.github.com>
2018-08-25 21:51:46 -0300

    Update CLIENT.php

--- CLIENT.php
+++ CLIENT.php
@@ -16,35 +16,59 @@ timestamp (hora da máquina com data)*/
 
 <?php
 //Definicao da porta e do endereco
-$endereco = '192.168.1.53';
-$porta2-1 = 10000;
+$endereco_server = '192.168.1.53';
+$porta_server = 10000;
 
 //Criar socket
-$socket_antrede = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+$socket_cliente = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 /*AF_INET é um parametro domain IPv4 baseado nos protocolos de Internet. TCP é protocolo comum dessa família de protocolos.*/
 /* SOCK_STREAM éFornece sequencial, seguro, e em ambos os sentidos, conexões baseadas em "byte streams". Dados "out-of-band" do
 mecanismo de transmissão devem ser suportados. O protocolo TCP é baseado neste tipo de socket*/
 
 //Verificar se a criacao do socket foi ok
-if ($socket_antrede === false){
+if ($socket_cliente === false){
   echo "/n------\nErro na criacao do socket: ".socket_strerror(socket_last_error())."\n------\n";
   //chama a função socket_strerror() e pega o código de erro com a função socket_last_error().
   //Retorna uma string descrevendo o erro.
 }
 else{
-  echo "Socket criado com sucesso!\n"; //Exibe uma string avisando que a criacao ocorreu bem
+	echo "Socket criado com sucesso!\n"; //Exibe uma string avisando que a criacao ocorreu bem
 }
-//Conexao com o servidor
+
+$contador=0;
+//Ocorrerao apenas 5 tentativas de conexao com o server
+while($contador<=5){
+	$contador=$contador+1;
+	//Conexao com o servidor - estabelecer uma conexão TCP entre cliente e servidor
+  echo "Tentando conectar ao '$endereco_server' pela porta '$porta_server' ... \n";
+  $conexao = socket_connect (resource $socket_cliente , string $endereco_server , int $porta_server);
+  if ($conexao === false){
+		if($contador>5){
+			echo "/n------\nErro no estabelecimento da conexao com o socket: ".socket_strerror(socket_last_error())."\n------\n";
+			//chama a função socket_strerror() e pega o código de erro com a função socket_last_error().
+			//Retorna uma string descrevendo o erro.
+		}
+		else{
+			echo "Tentando novamente...\n";
+		}
+  }
+  else{
+  	echo "Conexao estabelecida com sucesso!\n"; //Exibe uma string avisando que a conexao ocorreu bem
+		break;
+	}
+}
+//Atribui um nome ao socket
+socket_bind ($socket_cliente , $endereco_server, $porta_server);
+//Abre escuta para uma conexão no socket
+socket_listen ($socket_cliente, SOMAXCONN); //SOMAXCONN - valor máximo passado para o parâmetro backlog
+//Após o socket socket ter sido criado, passar um nome, e dizer para listar conexões, essa função irá aceitar conexões neste socket.
+//Uma vez que uma conexão com sucesso é feita, um novo "resource" do socket é retornado, que deve ser usado para comunicação. 
+socket_accept ( $socket_cliente);
+//Camada física recebe da camada superior a mensagem a ser trocada e o endereço (IP) do destinatário.
 
 //Probabilidade de colisão durante envio dos pacotes
-
-//Camada física recebe da camada superior a mensagem a ser trocada e o endereço (IP) do destinatário.
-//O programa deve estar continuamente em execucao: receber a quantidade de bits e enviar em pacotes
-while(true){
-  
-}
 //Encerra a conexão
-socket_close($socket_antrede);
+socket_close($socket_cliente);
 echo "Conexao encerrada!\n";
 ?>
 
