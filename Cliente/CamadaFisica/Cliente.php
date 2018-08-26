925b058d6915511960a94a430eddae93743a65f8
BulgarelliM <42696123+BulgarelliM@users.noreply.github.com>
2018-08-26 12:04:57 -0300

    Update Cliente.php

--- Cliente/CamadaFisica/Cliente.php
+++ Cliente/CamadaFisica/Cliente.php
@@ -63,25 +63,34 @@ function enviarMessagemServidor($socket, $mensagem)
 
 function receberRespostaServidor($socket, $limiteMensagem)
 {
-    $result = socket_read ($socket, $limiteMensagem) or die("Could not read server response\n");
-    echo " Reply From Server  :".$result . "\n";
-    return $result;
+    $result = socket_read ($socket, $limiteMensagem);// or die("Could not read server response\n");
+		if($result === false){
+			$timestamp = date("Y-m-d H:i:s");
+			file_put_contents($log_geral, "Socket_read --- Não foi possivel ler a resposta do server --- ".$timestamp."\n", FILE_APPEND);
+			exit("Could not read server response\n");
+		}
+		else{
+			echo " Reply From Server  :".$result . "\n";
+			$timestamp = date("Y-m-d H:i:s");
+			file_put_contents($log_geral, "Socket_read --- Reply From Server  :".$result ." --- ".$timestamp."\n", FILE_APPEND);
+			return $result;
+		}
 }
 
 function conectarAoServidor($host, $port, $mensagem, $limite)
 {
-    $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n"); //SOL_TCP
+    $socket = socket_create(AF_INET, SOCK_STREAM, 0);// or die("Could not create socket\n"); //SOL_TCP
     /*AF_INET é um parametro domain IPv4 baseado nos protocolos de Internet. TCP é protocolo comum dessa família de protocolos.*/
     /* SOCK_STREAM éFornece sequencial, seguro, e em ambos os sentidos, conexões baseadas em "byte streams". Dados "out-of-band" do
     mecanismo de transmissão devem ser suportados. O protocolo TCP é baseado neste tipo de socket*/
     //Verificar se a criacao do socket foi ok
     if ($socket === false){
-        echo "/n------\nErro na criacao do socket: ".socket_strerror(socket_last_error())."\n------\n";
-        //chama a função socket_strerror() e pega o código de erro com a função socket_last_error().
-        //Retorna uma string descrevendo o erro.
-        $timestamp = date("Y-m-d H:i:s");
-        file_put_contents($log_geral, "Criacao de socket --- Erro na criacao do socket do cliente --- ".$timestamp."\n", FILE_APPEND);
-	    //FILE_APPEND - Se o arquivo filename já existir, acrescenta os dados ao arquivo ao invés de sobrescrevê-lo.
+      $timestamp = date("Y-m-d H:i:s");
+      file_put_contents($log_geral, "Criacao de socket --- Erro na criacao do socket do cliente --- ".$timestamp."\n", FILE_APPEND);
+			//FILE_APPEND - Se o arquivo filename já existir, acrescenta os dados ao arquivo ao invés de sobrescrevê-lo.
+			exit ("/n------\nErro na criacao do socket: ".socket_strerror(socket_last_error())."\n------\n");
+			//chama a função socket_strerror() e pega o código de erro com a função socket_last_error().
+      //Retorna uma string descrevendo o erro.
     }
     else{
 	    echo "Socket criado com sucesso!\n"; //Exibe uma string avisando que a criacao ocorreu bem
