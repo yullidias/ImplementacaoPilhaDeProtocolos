9f3df3f64df56232afade8d1548ea3ea20489ef3
eduhumb <eduhumb@gmail.com>
2018-09-29 21:33:02 -0300

    application ruby files

--- README.md
+++
@@ -1,9 +0,0 @@
-# ImplemetacaoPilhaDeProtocolos
-Implementação das camadas do modelo TCP/IP
-# Pré-requisitos
-- PHP 7
-# Modo de execução
-- Execute o arquivo arquivo Servidor.php, em Servidor >> CamadaFisica , com o comando "php Servidor.php"
-- Execute o arquivo arquivo Cliente.php, em Cliente >> CamadaFisica , com o comando "php Cliente.php"
-# Resultados
-- Os resultados da execução estão descritos no arquivo "log.txt"
---
+++ Transporte/Cliente.rb
@@ -0,0 +1,24 @@
+require 'socket'
+require_relative 'functions'
+
+include Socket::Constants
+
+localhost = "127.0.1.1"
+port_address = 9003
+
+socket_cliente_aplicacao = Socket.new( AF_INET, SOCK_STREAM, 0 )
+time_stamp("Camada Aplicacao (CLIENTE): Criado socket com a camada inferior ", "log.txt")
+
+sockaddr = Socket.pack_sockaddr_in( port_address, localhost )
+socket_cliente_aplicacao.connect( sockaddr )
+time_stamp("Camada Aplicacao (CLIENTE): Socket conectado com host local", "log.txt")
+
+socket_cliente_aplicacao.puts "http://www.google.com/ HTTP/1.1\n\n"
+time_stamp("Camada Aplicacao (CLIENTE): Enviada mensagem para camada inferior", "log.txt")
+
+data = client.recvfrom( 10000 )[0].chomp
+puts "Mensagem recebida: '#{data}'"
+time_stamp("Camada Aplicacao (CLIENTE): recebida mensagem de da camadad inferior ", "log.txt")
+
+socket_cliente_aplicacao.close
+time_stamp("Camada Aplicacao (CLIENTE): Encerrando conexao " , "log.txt")
