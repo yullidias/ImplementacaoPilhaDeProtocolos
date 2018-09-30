48d901ccec10ffc80706be163fb3fde0345a9d2f
eduhumb <eduhumb@gmail.com>
2018-09-29 22:40:40 -0300

    application folders updated

--- Cliente/CamadaAplicacao/Cliente.rb
+++ Cliente/CamadaAplicacao/Cliente.rb
@@ -4,10 +4,23 @@ require_relative 'functions'
 include Socket::Constants
 
 localhost = "127.0.1.1"
-port_address = 3000
+port_address = 7000
 
 socket_cliente_aplicacao = Socket.new( AF_INET, SOCK_STREAM, 0 )
+time_stamp("Camada Aplicacao (CLIENTE): Criado socket com a camada inferior ", "log.txt")
+
 sockaddr = Socket.pack_sockaddr_in( port_address, localhost )
 socket_cliente_aplicacao.connect( sockaddr )
-socket_cliente_aplicacao.puts "Testando"
+time_stamp("Camada Aplicacao (CLIENTE): Socket conectado com host local", "log.txt")
+
+socket_cliente_aplicacao.puts "http://www.google.com/ HTTP/1.1\n\n"
+time_stamp("Camada Aplicacao (CLIENTE): Enviada mensagem para camada inferior", "log.txt")
+
+# data = client.recvfrom( 10000 )[0].chomp
+# puts "Mensagem recebida: '#{data}'"
+# time_stamp("Camada Aplicacao (CLIENTE): recebida mensagem de da camadad inferior ", "log.txt")
+
 socket_cliente_aplicacao.close
+time_stamp("Camada Aplicacao (CLIENTE): Encerrando conexao " , "log.txt")
+puts 'conexão encerrada com sucesso'
+
--- Servidor/CamadaAplicacao/Servidor.rb
+++ Servidor/CamadaAplicacao/Servidor.rb
@@ -4,26 +4,37 @@ require_relative 'functions'
 include Socket::Constants
 
 localhost = "127.0.1.1"
-port_address = 3000
+port_address = 7000
 
 socket_servidor_aplicacao = Socket.new( AF_INET, SOCK_STREAM, 0 )
-time_stamp("Camada Aplicacao: Criado socket com a camada inferior ", "../../log.txt")
+time_stamp("Camada Aplicacao (SERVIDOR): Criado socket com a camada inferior ", "log.txt")
 
 sockaddr = Socket.pack_sockaddr_in( port_address, localhost )
 socket_servidor_aplicacao.bind( sockaddr )
-time_stamp("Camada Aplicacao: Bind do socket com a camada inferior ", "../../log.txt")
+time_stamp("Camada Aplicacao (SERVIDOR): Bind do socket com a camada inferior ", "log.txt")
 
-socket_servidor_aplicacao.listen( 1 )
-time_stamp("Camada Aplicacao: Servidor escutando...", "../../log.txt")
+socket_servidor_aplicacao.listen( 5 )
+time_stamp("Camada Aplicacao (SERVIDOR): Servidor escutando...", "log.txt")
 
-client, client_addrinfo = socket_servidor_aplicacao.accept
-# puts client_addrinfo
+loop do
+  client, client_addrinfo = socket_servidor_aplicacao.accept
 
-write_to_file("ip.txt", client_addrinfo)
+  write_to_file("ip.txt", client_addrinfo)
 
-data = client.recvfrom( 10000 )[0].chomp
-puts "I only received 20 bytes '#{data}'"
-sleep 1
+  data = client.recvfrom( 10000 )[0].chomp
+  if data
+    puts "Mensagem recebida: '#{data}'"
+    time_stamp("Camada Aplicacao (SERVIDOR): recebida mensagem da camada inferior ", "log.txt")
+
+    sleep 1
+    # return "200"
+  else
+    sleep 1
+    # return "400"
+  end
+
+end
 
 socket_servidor_aplicacao.close
-time_stamp("Camada Aplicacao: Encerrando conexao " , "../../log.txt")
+time_stamp("Camada Aplicacao (SERVIDOR): Encerrando conexao " , "log.txt")
+
