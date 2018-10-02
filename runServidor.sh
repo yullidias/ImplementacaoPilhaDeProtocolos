7540eea5647ac51413846b8d0cf703e68fcdde9a
aluno <yulli.dias@hotmail.com>
2018-10-02 11:06:23 -0300

    matando processos caso existam

--- runServidor.sh
+++ runServidor.sh
@@ -4,6 +4,12 @@ then
 	echo "Use sudo ./runServidor.sh IpServidorDNS"
 	exit -1
 fi
+
+ps -aux | grep "ruby Servidor/CamadaAplicacao/Servidor.rb" | cut -d" " -f6 | xargs sudo kill -9
+ps -aux | grep "php Servidor/CamadaFisica/Servidor.php" | cut -d" " -f6 | xargs sudo kill -9
+ps -aux | grep "sudo php Cliente/CamadaFisica/Cliente.php" | cut -d" " -f6 | xargs sudo kill -9
+ps -aux | grep "php Cliente/CamadaFisica/Cliente.php" | cut -d" " -f6 | xargs sudo kill -9
+
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 echo -e "${RED}Caso mostre Warnings, use lsof -wni tcp:8070 && lsof -wni tcp:8080 && lsof -wni tcp:8090 e mate os processos! ${NC}"
