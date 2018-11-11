1059e11a0e0d785c2ee12ecfcda830f5b1e32c8a
yullidias <yulli.dias@hotmail.com>
2018-11-11 19:13:16 -0200

    mostrando pids dos processos

--- runServidor.sh
+++ runServidor.sh
@@ -13,6 +13,13 @@ ps -aux | grep "php Cliente/CamadaFisica/Cliente.php" | cut -d" " -f6 | xargs su
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 echo -e "${RED}Caso mostre Warnings, use lsof -wni tcp:8070 && lsof -wni tcp:8080 && lsof -wni tcp:8090 e mate os processos! ${NC}"
+echo "Executar esse script na raiz do repositorio"
 ruby Servidor/CamadaAplicacao/Servidor.rb "$1" &
+pidSR=$!
+echo $pidSR
 php Servidor/CamadaFisica/Servidor.php "$1" "$1" &
+pidSP=$!
+echo $pidSP
 sudo php Cliente/CamadaFisica/Cliente.php "$1" "$1" &
+pidCP=$!
+echo $pidCP
